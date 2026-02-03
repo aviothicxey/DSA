595bd28d4537a5b3da99145ffe9a5b85e65ce1c1
Aparna <gargaparna78@gmail.com>
2026-02-03 17:51:59 +0530

    init

--- Recursion.java
+++ Recursion.java
@@ -75,37 +75,78 @@ public class Recursion{
     }
     //print the subsequence
     static void Subseq(int ind , ArrayList<Integer> ds, int[] arr , int n){
-
+        if(ind >= n){
+            System.out.println(ds);
+            return;
+        }
+        ds.add(arr[ind]);
+        Subseq(ind +1 , ds ,arr , n);
+        ds.remove(ds.size()-1);
+        Subseq(ind+1 , ds,arr,n);
+    }
+    //print subsequenc whose sum is K:
+    static void SumK(int ind , ArrayList<Integer> ds , int sum ,int k , int[]arr , int n){
+        if(ind == n){
+            if(sum == k) System.out.println(ds);
+            return;
+        }
+        ds.add(arr[ind]);
+        SumK(ind +1 , ds , sum+arr[ind] ,k , arr,n);
+        ds.remove(ds.size()-1);
+        SumK(ind + 1, ds, sum , k, arr, n);
+    }
+    // count the subsequence whose sum is k
+    static int countSEq(int ind , int sum , int k , int[] arr , int n){
+        if(ind == n){
+            if(sum == k) return 1;
+            return 0;
+        }
+        sum += arr[ind];
+        int left = countSEq(ind+1, sum, k, arr, n);
+        sum -= arr[ind];
+        int right = countSEq(ind + 1, sum, k, arr, n);
+        return left + right;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt() ;
-         printName(n); // Aparna n times
-         pn(1,n); // ayush n times
-         printNum(1, n); //1 to n normal recursion
-         printn(n, n); //1 to n(backtracing)
-         bpn(1, n);//n to 1 (backtracking)
-         sumOfN(n,0);
-         int res = fun(n);
-         System.out.println(res);
-         int factorial = fact(n);
-         System.out.println(factorial);
-         int[] arr = new int[n];
-         for(int i = 0 ; i < n ; i++){
-            arr[i] = sc.nextInt();
-         }
-         reverseArray(0, n, arr);
-         for(int i = 0 ; i < n ; i++){
-            System.out.println(arr[i]);
-         }
-         String x = sc.next();
-         boolean p = isPalindrome(0, x.length(), x);
-         System.out.println(p);
-         System.out.println(fibonacci(5));
-         int v= 3;
-         int[] a = {3, 2,1};
-         ArrayList<Integer> ds = new ArrayList<>();
-         Subseq(0,ds,arr,n);
+        // int n = sc.nextInt() ;
+        //  printName(n); // Aparna n times
+        //  pn(1,n); // ayush n times
+        //  printNum(1, n); //1 to n normal recursion
+        //  printn(n, n); //1 to n(backtracing)
+        //  bpn(1, n);//n to 1 (backtracking)
+        //  sumOfN(n,0);
+        //  int res = fun(n);
+        //  System.out.println(res);
+        //  int factorial = fact(n);
+        //  System.out.println(factorial);
+        //  int[] arr = new int[n];
+        //  for(int i = 0 ; i < n ; i++){
+        //     arr[i] = sc.nextInt();
+        //  }
+        //  reverseArray(0, n, arr);
+        //  for(int i = 0 ; i < n ; i++){
+        //     System.out.println(arr[i]);
+        //  }
+        //  String x = sc.next();
+        //  boolean p = isPalindrome(0, x.length(), x);
+        //  System.out.println(p);
+        //  System.out.println(fibonacci(5));
+        //  int v= 3;
+        //  int[] a = {3, 2,1};
+        //  ArrayList<Integer> ds = new ArrayList<>();
+        //  Subseq(0,ds,a,v);
+
+        // int[] arr = {1,2,1};
+        // int k = 2;
+        // ArrayList<Integer> ds = new ArrayList<>();
+        // SumK(0, ds, 0, k, arr, arr.length);
+        int arr[] = {1,2,1};
+        int k = 2;
+        int n = arr.length;
+        int count = countSEq(0, 0, k, arr, n);
+        System.out.println(count);
+
          sc.close();
     }
     
