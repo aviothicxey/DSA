eba4b8d53b9dd77b7a5ce4076d97d20d7c7c3f05
Aparna <gargaparna78@gmail.com>
2026-03-04 18:18:33 +0530

    init

---
+++ array_recursion/rbs.java
@@ -0,0 +1,28 @@
+package array_recursion;
+
+public class rbs {
+    public static void main(String[]args){
+        int arr[] = {9,8,7,6,1,2,3};
+        int ans =findIndex(arr, 0, arr.length-1, 2);
+        System.out.println(ans);
+
+    }
+    static int findIndex(int arr[] , int s, int e , int target){
+        if(s < e) return -1;
+        int m = s + (e-s) / 2;
+        if(target == arr[m]) return m;
+        if(arr[s] <= arr[m]){
+            if(arr[s] <= target && arr[m] >= target){
+                return findIndex(arr, s, m-1, target);
+            }else{
+                return findIndex(arr , m+1 , e , target);
+            }
+        }else{
+            if(arr[s] <= target && arr[e] >= target){
+                return findIndex(arr , m+1 , e , target);
+            }else{
+                return findIndex(arr, s, m-1 , target);
+            }
+        }
+    }
+}
