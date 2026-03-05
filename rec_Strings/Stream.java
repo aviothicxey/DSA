5dc1c2413870f76d961790379fae50e7294c23f4
Aparna <gargaparna78@gmail.com>
2026-03-05 12:50:50 +0530

    init

--- rec_Strings/Stream.java
+++ rec_Strings/Stream.java
@@ -4,7 +4,10 @@ public class Stream {
     
     public static void main(String[] args){
         skip("" , "bbachadahaeerh");
+        String ans = skip2("Aparna");
+        System.out.println(ans);
     }
+
     static void skip(String a , String g){
 
         if(g.isEmpty()){
@@ -18,6 +21,18 @@ public class Stream {
             skip(a + ch , g.substring(1));
         }
 
+    }
+
+    static String skip2(String g){
+        if(g.isEmpty()){
+            return "";
+        }
+        char ch = g.charAt(0);
+        if(ch == 'a'){
+            return skip2(g.substring(1));
+        }else{
+            return ch +skip2(g.substring(1));
+        }
     }
 
 }
