b87a89b746f6673b694312ee88e7803ec25acf90
Aparna <gargaparna78@gmail.com>
2026-03-05 14:27:12 +0530

    init

--- rec_Strings/Stream.java
+++ rec_Strings/Stream.java
@@ -6,6 +6,8 @@ public class Stream {
         skip("" , "bbachadahaeerh");
         String ans = skip2("Aparna");
         System.out.println(ans);
+        System.out.println(skipApple("ansappleabc"));
+        System.out.println(skipAppNotApple("appappleabcappleapp"));
     }
 
     static void skip(String a , String g){
@@ -32,6 +34,25 @@ public class Stream {
             return skip2(g.substring(1));
         }else{
             return ch +skip2(g.substring(1));
+        }
+    }
+    static String skipApple(String g){
+        if(g.isEmpty()) return "";
+
+        if(g.startsWith("apple")){
+            return skipApple(g.substring(5));
+        }else{
+            return g.charAt(0) + skipApple(g.substring(1));
+        }
+    }
+
+    static String skipAppNotApple(String g){
+        if(g.isEmpty()) return "";
+
+        if(g.startsWith("app") && !g.startsWith("apple")){
+            return skipAppNotApple(g.substring(3));
+        }else{
+            return g.charAt(0) + skipAppNotApple(g.substring(1));
         }
     }
 
