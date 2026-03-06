33fe561d64be610c67be50afda2e8e2ed6ec12da
Aparna <gargaparna78@gmail.com>
2026-03-06 12:49:54 +0530

    init

--- rec_Strings/Subseq.java
+++ rec_Strings/Subseq.java
@@ -1,10 +1,14 @@
 package rec_Strings;
 
+import java.util.ArrayList;
+
 public class Subseq {
 
     public static void main(String[] args) {
-        SubSeq("","abc");
+       
+        System.out.println(SubSeqAsciiRet("", "abc"));
     }
+
     static void SubSeq(String a , String g){
         if(g.isEmpty()) {
             System.out.println(a);
@@ -16,6 +20,48 @@ public class Subseq {
         SubSeq(a, g.substring(1));
         
         
+    }
+    static ArrayList<String> SubSeqRet(String a , String g){
+        if(g.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(a);
+            return list;
+        }
+
+        char ch = g.charAt(0);
+        ArrayList<String> left = SubSeqRet(a+ch , g.substring(1)  );
+        ArrayList<String> right = SubSeqRet(a, g.substring(1));
+        left.addAll(right);
+        return left;
+    }
+    static  void SubSeqAscii(String a , String g){
+        if(g.isEmpty()) {
+            System.out.println(a);
+            return;
+        }
+
+        char ch = g.charAt(0);
+        SubSeqAscii(a+ch , g.substring(1)  );
+        SubSeqAscii(a, g.substring(1));
+        SubSeqAscii(a+(ch+0) , g.substring(1));
+        
+        
+    }
+    static ArrayList<String> SubSeqAsciiRet(String a , String g){
+         if(g.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(a);
+            return list;
+         }
+         char ch = g.charAt(0);
+         ArrayList<String> ist = SubSeqAsciiRet(a + ch, g.substring(1));
+         ArrayList<String> second = SubSeqAsciiRet(a , g.substring(1));
+         ArrayList<String> third = SubSeqAsciiRet(a + (ch +0) , g.substring(1));
+
+         ist.addAll(second);
+         ist.addAll(third);
+         return ist;
+
     }
     
 }
