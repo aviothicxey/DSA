dacc47c96d49a87d36d2aea17eae8d1c16a485ef
Aparna <gargaparna78@gmail.com>
2026-09-18 16:34:54 +0530

    init

--- practice/one.java
+++ practice/one.java
@@ -1,9 +1,7 @@
 package practice;
 
 public class one {
-    //fam = ori - dis + service
-    //dis = ori * dis%/100
-    //sc = amt after dis * sc%/100
+
 static int fun(String s){
     return s.length();
 }
